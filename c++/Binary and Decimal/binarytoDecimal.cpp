cca464bdc1bed983b475e50af27125fd4b1387b3
Ramasahayam Sreekaran Reddy <86601811+Sreekaran1704@users.noreply.github.com>
2021-11-28 15:16:09 +0530

    Create binarytoDecimal.cpp

---
+++ c++/Binary and Decimal/binarytoDecimal.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;int i=0;
+    int ans=0;
+    cin>>n;
+    while(n!=0){
+        int digit=n%10;
+        if(digit==1){
+        ans=ans+(pow(2,i));
+        }n=n/10;
+        i++;
+        
+    }cout<<ans;
+}
