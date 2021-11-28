a74bb462ed66a7a2adac78c4fc19878eaec90857
Ramasahayam Sreekaran Reddy <86601811+Sreekaran1704@users.noreply.github.com>
2021-11-28 10:16:26 +0530

    Add files via upload

---
+++ ifelse.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    if(n>0){
+        cout<<"It is positive";
+    }else{
+        cout<<"It is negative";
+    }
+}
